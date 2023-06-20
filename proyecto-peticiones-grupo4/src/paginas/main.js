22f3f2fa3fea85a7095018d488d24ff4adf34058
Guillermo Andre Espinal Sierra <guille.gaes@gmail.com>
2023-06-20 13:08:25 -0600

    Diseno de app modificado
    Diseno de app modificado

--- proyecto-peticiones-grupo4/src/paginas/main.js
+++ proyecto-peticiones-grupo4/src/paginas/main.js
@@ -1,22 +1,43 @@
 import React, { useState } from "react";
 import "./main.css"
+import img1 from "./icon.png"
 
 const Main =() => {
 
     const  [peticionReporte,setpeticionReporte]= useState("");
+    
+
+   
 
     const envioPeticion_Reporte = () => {
-        console.log("Se envio la peticion"+ peticionReporte);
+        console.log("Se envio la peticion: "+ peticionReporte);
+       
     }
+  
     return (
+        
         <div className="principal">
             <div className="cajaCentral">
-                <h1>Reportar problema</h1>
-                <textarea  type="text"   width= "100%" className="entradaPeticion" placeholder="Ingrese reporte sobre problema"
-                    onChange={(event) => setpeticionReporte(event.target.value)}/>
-                <button className="Sendbt" type="button" onClick={envioPeticion_Reporte}>Enviar</button>
+               
+                <h1 className = "text">Reportar problema</h1>
+                <div>
+                    <textarea   
+                                type="text"  
+                                className="entradaPeticion" 
+                                placeholder="Ingrese reporte sobre problema"
+                                onChange={(event) => setpeticionReporte(event.target.value)} 
+
+                                
+                    />
+                    <img src={img1} className = "image" />
+                </div>
+                <button className="Sendbt" type="button" onClick={() =>setpeticionReporte("")}>Enviar</button>
+                
+               
             </div>
         </div>
+        
+       
         
     )
 }
